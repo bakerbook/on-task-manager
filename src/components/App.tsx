5e1c0e8ba1511a5edf1b863b51c0f5d7463ea597
baker-1234 <bakerjbook@gmail.com>
2024-03-05 17:20:55 -0800

    Add the Lilita One font, organize folders, and start on UI

---
+++ src/components/App.tsx
@@ -0,0 +1,7 @@
+export default function App(){
+    return(
+        <div className="mx-36 mt-4 mb-72">
+            <h1 className="text-3xl font-bold text-center">On-Task Manager</h1>
+        </div>
+    )
+}
