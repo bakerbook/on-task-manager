b0e2ad13a6cedd9ebe2cafb6d8538774cb6f9f5c
baker-1234 <bakerjbook@gmail.com>
2024-03-05 17:25:28 -0800

    Change font location so there aren't any duplicates in the build

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,10 @@
+import ChoiceButtons from "./ChoiceButtons"
+
 export default function App(){
     return(
         <div className="mx-36 mt-4 mb-72">
             <h1 className="text-3xl font-bold text-center">On-Task Manager</h1>
+            <ChoiceButtons />
         </div>
     )
 }
---
+++ src/components/ChoiceButtons.tsx
@@ -0,0 +1,9 @@
+export default function ChoiceButtons(){
+    return(
+        <div>
+            <button>A</button>
+            <button>B</button>
+            <button>C</button>
+        </div>
+    )
+}
