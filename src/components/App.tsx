b106e8ba312a2a73337c73bc6d1b1ba079a84fe7
baker-1234 <bakerjbook@gmail.com>
2024-04-14 00:38:48 -0700

    Change sizing of extension and center everything

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,7 +2,7 @@ import ChoiceButtons from "./ChoiceButtons"
 
 export default function App(){
     return(
-        <div className="mx-36 mt-4 mb-72">
+        <div className="mx-27 mt-4 grid justify-items-center">
             <h1 className="text-3xl text-pastel-blue-green font-bold text-center">On-Task Manager</h1>
             <ChoiceButtons />
         </div>
